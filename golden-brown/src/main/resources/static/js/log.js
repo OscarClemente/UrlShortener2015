a8ca6ca64ccd9c997bd9e6af4714560f3e9e5c5b
Eduardo Ibáñez <eduiba94@gmail.com>
2015-12-01 10:31:21 +0100

    buttons inline fixed

--- golden-brown/src/main/resources/static/js/log.js
+++ golden-brown/src/main/resources/static/js/log.js
@@ -2,9 +2,9 @@ $(document).ready(
 	function() {
 		$("#log_out").on("click", function(event) {
 			sessionStorage.removeItem('usuario');
-			document.getElementById("log_in").style.visibility="block";
-			document.getElementById("register").style.visibility="block";
-			document.getElementById("user").style.visibility="none";
-	    	document.getElementById("log_out").style.visibility="none";
+			document.getElementById("log_in").style.display="inline";
+			document.getElementById("register").style.display="inline";
+			document.getElementById("user").style.display="none";
+	    	document.getElementById("log_out").style.display="none";
 		});
 	});
